7cc8139170b99319feee0b2874a7959d2f9a5300
Garret Gallant <gallanttechnological@gmail.com>
2020-07-29 17:43:33 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,8 @@ def find_element_index(array, value_to_find)
   count = 0
   
   while count < array.length do
-    if array[count] == value_to_find
-      array[count]
-  else count += 1
+    puts array[count] == value_to_find
+    
+   count += 1
   end
 end
